93a3b3859b9f07e376bde003f9c47516f5507095
sakito <sakito@sakito.com>
2025-01-13 10:24:46 +0900

    waitress に変更

--- ui/app.py
+++ ui/app.py
@@ -6,8 +6,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def hello_world():
-    return "<p>Hello, World!</p>"
-
+    return '<p>Hello, World! 確認用画面</p>'
 
